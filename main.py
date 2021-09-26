e2a82bb602436b4695175075fe4c364d3c766c91
Gelli13 <horvath.gellert.endre.tech2020b@bolyaimovar.com>
2021-09-26 17:07:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+time = 2.7/50+7.9/90+29.7/130
+hour = time
+minute = 60*time
+print("a.) feladat:")
+print(20*"-")
+print("Az út ennyi ideig tart:", round(time, 2), "óra")
+print()
+print("b.) feladat:")
+print(20*"-")
+print("Az út ennyi ideig tart:", round(minute), "perc")
+print()
+print("c.) feladat:")
+print(20*"-")
+print("Ekkor kell elindulnia Dórinak az egyetemre, hogy odaérjen 8órára az egyetemre:", "7:38")
+
